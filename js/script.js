0c5c3500b80130b0b110b5714e6b771be164a5dd
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-15 18:09:15 +0100

    create function for collection of user info-division

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 // using prompt to collect user input for subtraction
-let number1 = parseInt(prompt("type1"));
-let number2 = parseInt(prompt(type2));
+// let number1 = parseInt(prompt("type1"));
+// let number2 = parseInt(prompt("type2"));
 
-function sub(number1, number2){
-    return number1 - number2
-}
-console.log(sub(number1, number2));
+// function sub(number1, number2){
+//     return number1 - number2;
+// }
+// console.log(sub(number1, number2));
+// document.write(sub(number1, number2));
+// alert(sub(number1, number2));
+
+// using prompt to collect user input for division
+// let number1 = parseInt(prompt("first number"));
+// let number2 = parseInt(prompt("second number"));
+// let number3 = parseInt(prompt("third number"));
+
+// function div(number1, number2, number3){ 
+//     return number1 / number2 / number3;
+// }
+// console.log(div(number1, number2, number3));
+// document.write(div(number1, number2, number3));
+// alert(div(number1, number2, number3));
