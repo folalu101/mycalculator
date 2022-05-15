97c06c8d2a399d987a6157748a7b2951af3723a3
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-15 18:25:10 +0100

    create function for collection of user info-multiplication

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@
 // document.write(sub(number1, number2));
 // alert(sub(number1, number2));
 
+
+
 // using prompt to collect user input for division
 // let number1 = parseInt(prompt("first number"));
 // let number2 = parseInt(prompt("second number"));
@@ -20,3 +22,16 @@
 // console.log(div(number1, number2, number3));
 // document.write(div(number1, number2, number3));
 // alert(div(number1, number2, number3));
+
+
+// using prompt to collect user input for division
+let number1 = parseInt(prompt("first number"));
+let number2 = parseInt(prompt("second number"));
+let number3 = parseInt(prompt("third number"));
+
+function multiply(number1, number2, number3){
+    return number1 * number2 * number3;
+}
+console.log(multiply(number1, number2, number3));
+document.write(multiply(number1, number2, number3));
+alert(multiply(number1, number2, number3));
