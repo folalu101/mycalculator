c031433d04734e1e8a8181f322f663dd93826d85
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-15 17:53:38 +0100

    create function for collection of user into- subtraction

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// using prompt to collect user input for subtraction
+let number1 = parseInt(prompt("type1"));
+let number2 = parseInt(prompt(type2));
+
+function sub(number1, number2){
+    return number1 - number2
+}
+console.log(sub(number1, number2));
